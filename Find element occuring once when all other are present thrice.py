ef90c4685733f2a635dba9e76a834d03c16a573d
Devyaansh Dwivedi <91078095+devyaanshdwivedi@users.noreply.github.com>
2024-01-04 23:00:03 +0530

    Add files via upload

---
+++ Find element occuring once when all other are present thrice.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def singleElement(self, arr, N):
+        # code here 
+        hashmap={}
+        for i in range(N):
+            if arr[i] not in hashmap:
+                hashmap[arr[i]]=1
+            else:
+                hashmap[arr[i]]+=1
+        key=list(hashmap.keys())
+        val=list(hashmap.values())
+        return key[val.index(1)]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        arr=list(map(int,input().split()))
+        
+        ob = Solution()
+        print(ob.singleElement(arr,N))
+# } Driver Code Ends
